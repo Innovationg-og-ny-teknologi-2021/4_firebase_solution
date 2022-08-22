4b1f1b539e01fb9149aa418b34f6138783ad3cd6
sero15af <sero15af@student.cbs.dk>
2022-08-22 10:30:12 +0200

    components added

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import SignUpForm from './components/SignUpForm';
+import firebase from 'firebase';
+import LoginForm from "./components/LoginForm";
+import ProfileScreen from "./components/ProfileScreen";
+import { Card } from 'react-native-paper';
+
+// Herunder foregår konfiguration til firebase. .
+const firebaseConfig = {
+  apiKey: "AIzaSyAts5zYxJljKY67teZC8XpxV-JNXNy_pzs",
+  authDomain: "oevelse5updated.firebaseapp.com",
+  projectId: "oevelse5updated",
+  storageBucket: "oevelse5updated.appspot.com",
+  messagingSenderId: "215574406693",
+  appId: "1:215574406693:web:967941b372d7eee9f041cc",
+  measurementId: "G-1QX8B2E9H7"
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+//Her oprettes bruger state variblen
+  const [user, setUser] = useState({ loggedIn: false });
+
+  //Koden sikrer at kun én Firebase initieres under brug af appen.
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+//onAuthstatechanged er en prædefineret metode, forsynet af firebase, som konstant observerer brugerens status (logget ind vs logget ud)
+//Pba. brugerens status foretages et callback i form af setUSer metoden, som håndterer user-state variablens status.
+  function onAuthStateChange(callback) {
+    return firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        callback({loggedIn: true, user: user});
+      } else {
+        callback({loggedIn: false});
+      }
+    });
+  }
+
+  //Heri aktiverer vi vores listener i form af onAuthStateChanged, så vi dynamisk observerer om brugeren er aktiv eller ej.
+  useEffect(() => {
+    const unsubscribe = onAuthStateChange(setUser);
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+//Her oprettes gæstekomponentsindhold, der udgøres af sign-up og login siderne
+  const GuestPage = () => {
+    return(
+        <View style={styles.container}>
+          <Text style={styles.paragraph}>
+            Opret eller Login med din firebase Email
+          </Text>
+
+          <Card style={{padding:20}}>
+            <SignUpForm />
+          </Card>
+
+          <Card style={{padding:20}}>
+            <LoginForm />
+          </Card>
+
+        </View>
+    )
+  }
+
+  return user.loggedIn ? <ProfileScreen /> : <GuestPage/> ;
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: '5%',
+    backgroundColor: 'transparent',
+    padding: 20,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
